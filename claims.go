7afbc0ce7290c38a69af597027b734ade4b15121
Phil Lu <wlu@infoblox.com>
2020-12-15 20:44:40 -0800

    added cmd/jwt-builder

--- claims.go
+++ claims.go
@@ -8,8 +8,8 @@ import (
 type Subject struct {
 	Id                 string `json:"id,omitempty"`                // user_email if authentication_type=(user_jwt|api_key)
 	SubjectType        string `json:"subject_type"`                //valid values: user/s2s
-	AuthenticationType string `json:"authentication_type"`         //valid values: bearer/token
 	AuthenticationId   string `json:"authentication_id,omitempty"` // the id of the api_key if authentication_type=token
+	AuthenticationType string `json:"authentication_type"`         //valid values: bearer/token
 }
 
 // Claims models the claims that atlas authz cares about.
